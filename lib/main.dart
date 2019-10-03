456ecee9ddbace757752224d9d9a1ce11e5e4822
Anisul Islam <air.rohit999@gmail.com>
2019-10-03 12:05:51 +0600

    Initializing project

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Expense Tracker',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Expense Tracker',
+        ),
+      ),
+      body: Center(
+          child: Text(
+        'Playground',
+      )),
+    );
+  }
+}
