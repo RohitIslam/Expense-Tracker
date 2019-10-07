639aa0dbd20729bec47e3811b4a875e1d8cd0fce
Anisul Islam <air.rohit999@gmail.com>
2019-10-07 20:24:22 +0600

    Configured chartbar hieghts dynamically using LayoutBuilder

--- lib/widgets/chart_bar.dart
+++ lib/widgets/chart_bar.dart
@@ -9,53 +9,62 @@ class ChartBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Container(
-          height: 20,
-          child: FittedBox(
-            child: Text(
-              'tk ${spendingAmount.toStringAsFixed(0)}',
+    return LayoutBuilder(
+      builder: (ctx, constrains) {
+        return Column(
+          children: <Widget>[
+            Container(
+              height: constrains.maxHeight * 0.15,
+              child: FittedBox(
+                child: Text(
+                  'tk ${spendingAmount.toStringAsFixed(0)}',
+                ),
+              ),
             ),
-          ),
-        ),
-        SizedBox(
-          height: 4,
-        ),
-        Container(
-          height: 60,
-          width: 10,
-          child: Stack(
-            children: <Widget>[
-              Container(
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.grey,
-                    width: 1.0,
+            SizedBox(
+              height: constrains.maxHeight * 0.05,
+            ),
+            Container(
+              height: constrains.maxHeight * 0.6,
+              width: 10,
+              child: Stack(
+                children: <Widget>[
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.grey,
+                        width: 1.0,
+                      ),
+                      color: Color.fromRGBO(220, 220, 220, 1),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
                   ),
-                  color: Color.fromRGBO(220, 220, 220, 1),
-                  borderRadius: BorderRadius.circular(10),
+                  FractionallySizedBox(
+                    heightFactor: spendingPercentageOfTotal,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Theme.of(context).primaryColor,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: constrains.maxHeight * 0.05,
+            ),
+            Container(
+              height: constrains.maxHeight * 0.15,
+              child: FittedBox(
+                child: Text(
+                  label,
                 ),
               ),
-              FractionallySizedBox(
-                heightFactor: spendingPercentageOfTotal,
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Theme.of(context).primaryColor,
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-        SizedBox(
-          height: 4,
-        ),
-        Text(
-          label,
-        ),
-      ],
+            ),
+          ],
+        );
+      },
     );
   }
 }
